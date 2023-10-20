4e61e6f7b472625f1078a18748086c37e0801400
benf-le <banglebang2016@gmail.com>
2023-10-21 02:16:08 +0700

    update

---
+++ client/app/(dashboard)/_components/sidebar.tsx
@@ -0,0 +1,8 @@
+//_componenst là một quy ước mới, các component chỉ được dùng trong (dáhboard) mà nókhông được dùng ra bên ngoài
+export const Sidebar=()=>{
+    return(
+        <div className="h-full border-r flex flex-col overflow-y-auto bg-white shadow-sm">
+            sidebar!
+        </div>
+    )
+}
