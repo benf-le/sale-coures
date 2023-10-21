a8942f6deb77eba5a269420abb585195568df784
benf-le <banglebang2016@gmail.com>
2023-10-21 16:02:49 +0700

    update

--- client/app/(dashboard)/(routes)/teacher/create/page.tsx
+++ client/app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -1,8 +1,24 @@
 "use client";
 
 import * as z from "zod";
-import {useForm} from "react-hook-form";
+import { useForm} from "react-hook-form";
 import {zodResolver} from "@hookform/resolvers/zod";
+import axios from "axios";
+import {useRouter} from "next/navigation";
+import toast from "react-hot-toast";
+
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormLabel,
+    FormMessage,
+    FormItem,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
 
 
 const formSchema = z.object({
@@ -13,7 +29,7 @@ const formSchema = z.object({
 })
 
 const CreatePage = () => {
-
+    const router = useRouter();
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues:{
@@ -22,13 +38,78 @@ const CreatePage = () => {
     })
 
     const {isSubmitting, isValid} = form.formState
-    const onSubmit =(values: z.infer<typeof formSchema>)=>{
-        console.log(values)
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try {
+            const reponse = await axios.post("/api/course", values)
+            router.push(`/teacher/course/${reponse.data.id}`)
+        } catch {
+            toast.error("Something went wrong")
+        }
     }
 
     return(
-        <div>
-            Create Page
+        <div className="max-w-5xl mx-auto flex md:items-center md:justify-center h-full p-6">
+            <div>
+                <h1 className="text-2xl">
+                    Name your course
+                </h1>
+
+                <p className="text-sm text-slate-600">
+                    What you like to name your course? Don&apos;t worry, you can change this later.
+                </p>
+
+                <Form {...form}>
+                    <form
+                        onSubmit={form.handleSubmit(onSubmit)}
+                        className="space-y-8 mt-8">
+
+                        <FormField
+                            control={form.control}
+                            name="title"
+                            render={({field})=>(
+                                <FormItem>
+                                    <FormLabel>
+                                        Course Title
+                                    </FormLabel>
+
+                                    <FormControl>
+                                        <Input
+                                        disabled={isSubmitting}
+                                        placeholder="e.g. Introduction to Computer Science"
+                                        {...field}
+                                        />
+                                    </FormControl>
+
+                                    <FormDescription>
+                                        What will teacher in this course?
+                                    </FormDescription>
+
+                                    <FormMessage/>
+                                </FormItem>
+                            )}/>
+
+                        <div className="flex items-center gap-x-2">
+                            <Link href="/">
+                                <Button
+                                    type="button"
+                                    variant="ghost"
+                                >
+                                    Cancel
+                                </Button>
+                            </Link>
+
+                                <Button
+                                    type="submit"
+                                    disabled={!isValid || isSubmitting}
+                                >
+                                    Continue
+                                </Button>
+
+
+                        </div>
+                    </form>
+                </Form>
+            </div>
         </div>
     )
 }
