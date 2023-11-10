243a9dcbce2e7cda14aad954dc9d634973e598b8
benf-le <banglebang2016@gmail.com>
2023-11-11 03:42:02 +0700

    update

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        domains: ['utfs.io']
+    images: {
+        domains: [
+            "utfs.io"
+        ]
     }
 }
 
