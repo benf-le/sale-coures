9a461c0e23bc18bd964204e7fd4fe16764cf5332
benf-le <banglebang2016@gmail.com>
2023-11-18 03:48:26 +0700

    update

--- client/app/(dashboard)/(routes)/search/page.tsx
+++ client/app/(dashboard)/(routes)/search/page.tsx
@@ -1,27 +1,53 @@
-import {db} from "@/lib/db";
-import {Categories} from "@/app/(dashboard)/(routes)/search/_components/categories";
-import {SearchInput} from "@/components/search-input";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-const SearchPage = async ()=>{
+import { db } from "@/lib/db";
+import { SearchInput } from "@/components/search-input";
+import { getCourses } from "@/actions/get-courses";
+import { CoursesList } from "@/components/courses-list";
+
+import { Categories } from "./_components/categories";
+
+interface SearchPageProps {
+    searchParams: {
+        title: string;
+        categoryId: string;
+    }
+};
+
+const SearchPage = async ({
+                              searchParams
+                          }: SearchPageProps) => {
+    const { userId } = auth();
+
+    if (!userId) {
+        return redirect("/");
+    }
 
     const categories = await db.category.findMany({
-        orderBy:{
-            name: 'asc'
+        orderBy: {
+            name: "asc"
         }
-    })
+    });
 
-    return(
+    const courses = await getCourses({
+        userId,
+        ...searchParams,
+    });
+
+    return (
         <>
             <div className="px-6 pt-6 md:hidden md:mb-0 block">
                 <SearchInput />
             </div>
-            <div>
-            <Categories
-                items={categories}
-            />
-        </div>
+            <div className="p-6 space-y-4">
+                <Categories
+                    items={categories}
+                />
+                <CoursesList items={courses} />
+            </div>
         </>
-    )
+    );
 }
 
-export default SearchPage
+export default SearchPage;
