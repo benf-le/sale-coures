95e246df79d898697ee854f9fa8ef28f85666a9d
benf-le <banglebang2016@gmail.com>
2023-11-18 00:40:29 +0700

    update

--- client/components/search-input.tsx
+++ client/components/search-input.tsx
@@ -9,9 +9,35 @@ import { Input } from "@/components/ui/input";
 import { useDebounce } from "@/hooks/use-debounce";
 
 export const SearchInput = () => {
+    const [value, setValue] = useState("");
+    const debouncedValue = useDebounce(value);
+
+
+    const searchParams = useSearchParams();
+    const router = useRouter();
+    const pathname = usePathname();
+
+    const currentCategoryId = searchParams.get("categoryId");
+
+    useEffect(() => {
+        const url = qs.stringifyUrl({
+            url: pathname,
+            query
+        })
+    })
+
+
+
 return(
-    <div>
-        kjg
+    <div className="realative">
+        <Search
+            className="h-4 w-4 absolute top-3 left-3 text-slate-600"
+        />
+        <Input
+            className="w-full md:w-[300px] pl-9 rounded-full bg-slate-100 focus-visible:ring-slate-200"
+            placeholder="Search for a course"
+        />
+
     </div>
 )
 }
